3862a5fe501aa81e415157906a2f80c53e669ccf
pollipolinch <pollifriend@mail.ru>
2023-05-08 03:13:03 +0500

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const folderLink = path.join(__dirname, 'files');
+const newFolderLink = path.join(__dirname, 'files-copy');
 
+fs.readdir(folderLink,{ withFileTypes: true },(errors, data) => {
+    if (errors) throw errors;
+    fs.mkdir(newFolderLink, { recursive: true}, errors => {
+        if (errors) return console.error(errors.message);
+      data.forEach(file => {
+        const fileLink = path.join(__dirname, 'files', file.name);
+        const newFileLink = path.join(__dirname, 'files-copy', file.name);
+        fs.copyFile(fileLink, newFileLink, (errors) => {
+            if (errors) return console.error(errors.message);
+        });
+      });
+    }); 
+  }
+);
